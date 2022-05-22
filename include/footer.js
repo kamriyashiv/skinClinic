489eb147398ac526b9a375ba638ed49a42a47d02
kamriyashiv <78133211+kamriyashiv@users.noreply.github.com>
2022-05-21 23:14:51 -0700

    Add files via upload

---
+++ include/footer.js
@@ -0,0 +1,43 @@
+var footer=document.getElementById("footer");
+	footer.innerHTML=`
+			<div class="mainFooter pt-5 text-white">
+					<div class="container">
+						<div class="row pt-5" >
+							<div class="col-md-3">
+								<h3 class="fs-5"><strong>ADRESSE</strong></h3>
+								<ul class="list-unstyled">
+									<li>Wilmersdorfer Straße 98-99</li>
+									<li>10629 Berlin</li>
+									<li>Deutschland</li>
+								</ul>
+							</div>
+							<div class="col-md-3">
+								<h3 class="fs-5"><strong>CONTACT</strong></h3>
+								<ul class="list-unstyled">
+									<li>Tel.: 030 253 303 26</li>
+									<li>Mob.: 0176 80642782</li>
+									<li>E-mail: info@myskinclinic.de</li>
+								</ul>
+							</div>
+							<div class="col-md-3">
+								<h3 class="fs-5"><strong>OPENING HOURS</strong></h3>
+								<ul class="list-unstyled">
+									<li>Mon-Fri by appointment</li>
+									<li>Online appointment</li>
+								</ul>
+							</div>
+							<div class="col-md-3">
+								<h3 class="fs-5"><strong>INFORMATION</strong></h3>
+								<ul class="list-unstyled">
+									<li>Impressum</li>
+									<li>10629 Berlin</li>
+									<li>Datenschutzerklärung</li>
+								</ul>
+							</div>
+						</div>
+						<div class="text-center text-whites py-4">
+							<p>© M.Y. Skin Clinic, 2020</p>
+						</div>
+					</div>
+				</div>
+	`;
